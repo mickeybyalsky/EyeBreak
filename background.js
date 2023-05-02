1fcf7301da4abb8e2ca8d88f2bc2c5a04d3b3ec5
mickeybyalsky <mickeybyalsky@gmail.com>
2023-05-01 21:04:13 -0400

    added test notification

---
+++ background.js
@@ -0,0 +1,147 @@
+// Set the default state of the timer
+
+let timer = {
+    running: false,
+    paused: false,
+    timeRemaining: 0,
+    intervalId: null
+  };
+  
+  // Start the timer
+  function startTimer() {
+    chrome.alarms.create("breakReminder", {
+        periodInMinutes: 20
+    });
+
+    chrome.alarms.onAlarm.addListener((alarm) => {
+        if (alarm.name === "breakReminder") {
+            displayNotif(); 
+  }
+  
+//     timer.running = true;
+//     timer.paused = false;
+//     timer.timeRemaining = 60;
+//     timer.intervalId = setInterval(function() {
+//       timer.timeRemaining--;
+//       if (timer.timeRemaining <= 0) {
+//         // Time's up, show a notification
+//         chrome.notifications.create({
+//           type: 'basic',
+//           iconUrl: 'icon.png',
+//           title: 'Eye break!',
+//           message: 'It\'s time to take a break and rest your eyes.',
+//           requireInteraction: true
+//         });
+//         clearInterval(timer.intervalId);
+//         timer.running = false;
+//         timer.paused = false;
+//         timer.timeRemaining = 0;
+//       } else {
+//         // Update the timer display in the popup
+//         chrome.runtime.sendMessage({
+//           action: 'updateTimerDisplay',
+//           timeRemaining: timer.timeRemaining
+//         });
+//       }
+//     }, 1000);
+//   }
+  
+  // Pause the timer
+  function pauseTimer() {
+    if (timer.running && !timer.paused) {
+      clearInterval(timer.intervalId);
+      timer.paused = true;
+    } else if (timer.running && timer.paused) {
+      timer.intervalId = setInterval(function() {
+        timer.timeRemaining--;
+        if (timer.timeRemaining <= 0) {
+          // Time's up, show a notification
+          chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: 'Eye break!',
+            message: 'It\'s time to take a break and rest your eyes.',
+            requireInteraction: true
+          });
+          clearInterval(timer.intervalId);
+          timer.running = false;
+          timer.paused = false;
+          timer.timeRemaining = 0;
+        } else {
+          // Update the timer display in the popup
+          chrome.runtime.sendMessage({
+            action: 'updateTimerDisplay',
+            timeRemaining: timer.timeRemaining
+          });
+        }
+      }, 1000);
+      timer.paused = false;
+    }
+  }
+  
+  // Stop the timer
+  function stopTimer() {
+    if (timer.running) {
+      clearInterval(timer.intervalId);
+      timer.running = false;
+      timer.paused = false;
+      timer.timeRemaining = 0;
+      chrome.runtime.sendMessage({
+        action: 'updateTimerDisplay',
+        timeRemaining: 0
+      });
+    }
+  }
+  
+  function displayNotif(){
+    console.log("displayNotif")
+    const options = {
+        type: 'progress',
+        title: 'Time for a break!',
+        message: 'Take a 20-second break and look away from your screen.',
+        iconUrl: 'images/eyeSmall.png',
+        priority: 2,
+        progress: 0
+      };
+      audio = new Audio('audio_file.mp3').play();
+      chrome.notifications.create(options, function(notificationId) {
+        console.log('Notification created:', notificationId);
+        let progress = 0;
+        const intervalId = setInterval(() => {
+          if (progress < 100) {
+            progress += 5;
+            chrome.notifications.update(notificationId, { progress });
+          } else {
+            clearInterval(intervalId);
+            chrome.notifications.clear(notificationId);
+          }
+        }, 1000);
+      });
+  }
+  // Listen for messages from the popup
+//   chrome.runtime.onMessage.addListener(
+//     function (request, sender, sendResponse) {
+//     console.log(request.message)
+//     if (request.action == 'startTimer') {
+//         startTimer();
+//     } else if (request.action == 'pauseTimer') {
+//         pauseTimer();
+//     } else if (request.action == 'stopTimer') {
+//         stopTimer();
+//     } else if (request.action == 'testNotif') {
+//         displayNotif(); 
+//     }
+//     sendResponse({message: "Hello from background.js!"});
+//   });
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request);
+        if (request.action)
+            displayNotif();
+
+        sendResponse(() => {
+            return false
+        });
+    }
+);
+    })};
--- popup.js
+++ popup.js
@@ -1,1 +1,88 @@
-console.log("This is a popup!")
+//startTimer
+const startTimer = document.getElementById("startTimer");
+startTimer.addEventListener("click", function() {
+    console.log("startTimer clicked!");
+    chrome.runtime.sendMessage({action: 'startTimer'});
+});
+
+//pauseTimer
+const pauseTimer = document.getElementById("pauseTimer");
+pauseTimer.addEventListener("click", function() {
+    console.log("pauseTimer clicked!");
+    chrome.runtime.sendMessage({action: 'pauseTimer'});
+});
+
+//stopTimer
+const stopTimer = document.getElementById("stopTimer");
+stopTimer.addEventListener("click", function() {
+    console.log("stopTimer clicked!");
+    chrome.runtime.sendMessage({action: 'stopTimer'});
+});
+
+
+//testNotif
+const testNotif = document.getElementById("testNotif");
+// testNotif.addEventListener('click', () => {
+//     chrome.runtime.sendMessage({action: 'testNotif'}, function(response){
+//         console.log(response.message);
+//     });
+// });
+testNotif.addEventListener("click", () => {
+    chrome.runtime.sendMessage({action: 'testNotif'}, function (response) {
+        console.log(response);
+    });
+});
+// function() {
+//     console.log("testNotif clicked!");
+//     chrome.runtime.sendMessage({action: 'testNotif'}), function(response){
+//         console.log(response.message);
+//     }
+    
+
+
+// testNotif
+// const testNotif = document.getElementById("testNotif");
+// testNotif.addEventListener('click', function() {
+//   console.log("testNotif clicked!");
+//   const options = {
+//     type: 'progress',
+//     title: 'Time for a break!',
+//     message: 'Take a 20-second break and look away from your screen.',
+//     iconUrl: 'images/eyeSmall.png',
+//     priority: 2,
+//     progress: 0
+//   };
+
+//   chrome.notifications.create(options, function(notificationId) {
+//     console.log('Notification created:', notificationId);
+//     let progress = 0;
+//     const intervalId = setInterval(() => {
+//       if (progress < 100) {
+//         progress += 5;
+//         chrome.notifications.update(notificationId, { progress });
+//       } else {
+//         clearInterval(intervalId);
+//         chrome.notifications.clear(notificationId);
+//       }
+//     }, 1000);
+//   });
+// });
+
+
+
+
+
+// testNotif.addEventListener("click", function() {
+//     console.log("testNotif clicked!");
+//     chrome.runtime.sendMessage({action: 'testNotif'});
+// });
+
+
+// ele.addEventListener("click", () => {
+//     chrome.runtime.sendMessage({ time: "1" }, function (response) {
+//         console.log(response);
+//     });
+// });
+
+
+
