b6c41ad48e28278e5d436a2405f74f6571801c82
mickeybyalsky <mickeybyalsky@gmail.com>
2023-05-02 19:32:27 -0400

    improved background script and added readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # EyeBreak
+The EyeBreak Chrome Extension is a tool designed to help users reduce eye strain and improve their overall well-being by reminding them to take regular breaks from their computer screen. 
+
+EyeBreak reminds users to practice the optometrist recommended 20-20-20 rule which suggests to focus your eyes on something at least 20 feet away, for a total of 20 seconds, for every 20 minutes spent using a screen.
+
+The extension allows users to set a timer for 20 minutes, after which a notification will remind them to take a break. The timer will repeat every 20 minutes with a 20-second break before the next alarm starting for 20 minutes.
+
+This rule is essential for everyone today, as almost everyone uses a computer screen everyday. Following this rule would mean less eye strain, better eyesight and less headaches.
--- popup.js
+++ popup.js
@@ -1,83 +1,36 @@
+chrome.action.setBadgeText({ text: 'OFF' });
 //startTimer
 const startTimer = document.getElementById("startTimer");
-startTimer.addEventListener("click", function() {
-    console.log("startTimer clicked!");
-    chrome.runtime.sendMessage({action: 'startTimer'});
+startTimer.addEventListener("click", function () {
+  console.log("startTimer clicked!");
+  chrome.runtime.sendMessage({ action: "startTimer" }, function (response) {
+    console.log(response);
+  });
 });
 
 //pauseTimer
 const pauseTimer = document.getElementById("pauseTimer");
-pauseTimer.addEventListener("click", function() {
-    console.log("pauseTimer clicked!");
-    chrome.runtime.sendMessage({action: 'pauseTimer'});
+pauseTimer.addEventListener("click", function () {
+  console.log("pauseTimer clicked!");
+  chrome.runtime.sendMessage({ action: "pauseTimer" }, function (response) {
+    console.log(response);
+  });
 });
 
 //stopTimer
 const stopTimer = document.getElementById("stopTimer");
-stopTimer.addEventListener("click", function() {
-    console.log("stopTimer clicked!");
-    chrome.runtime.sendMessage({action: 'stopTimer'});
+stopTimer.addEventListener("click", function () {
+  console.log("stopTimer clicked!");
+  chrome.runtime.sendMessage({ action: "stopTimer" }, function (response) {
+    console.log(response);
+  });
 });
-
 
 //testNotif
 const testNotif = document.getElementById("testNotif");
-
 testNotif.addEventListener("click", () => {
-    console.log("testNotif clicked!");
-    chrome.runtime.sendMessage({action: "testNotif"}, (response) => {
-        // console.log(response.message);
-    })
-    console.log("testNotif2 clicked!");
-    
+  console.log("testNotif clicked!");
+  chrome.runtime.sendMessage({ action: "testNotif" }, function (response) {
+    console.log(response);
+  });
 });
-
-    
-
-
-// testNotif
-// const testNotif = document.getElementById("testNotif");
-// testNotif.addEventListener('click', function() {
-//   console.log("testNotif clicked!");
-//   const options = {
-//     type: 'progress',
-//     title: 'Time for a break!',
-//     message: 'Take a 20-second break and look away from your screen.',
-//     iconUrl: 'images/eyeSmall.png',
-//     priority: 2,
-//     progress: 0
-//   };
-
-//   chrome.notifications.create(options, function(notificationId) {
-//     console.log('Notification created:', notificationId);
-//     let progress = 0;
-//     const intervalId = setInterval(() => {
-//       if (progress < 100) {
-//         progress += 5;
-//         chrome.notifications.update(notificationId, { progress });
-//       } else {
-//         clearInterval(intervalId);
-//         chrome.notifications.clear(notificationId);
-//       }
-//     }, 1000);
-//   });
-// });
-
-
-
-
-
-// testNotif.addEventListener("click", function() {
-//     console.log("testNotif clicked!");
-//     chrome.runtime.sendMessage({action: 'testNotif'});
-// });
-
-
-// ele.addEventListener("click", () => {
-//     chrome.runtime.sendMessage({ time: "1" }, function (response) {
-//         console.log(response);
-//     });
-// });
-
-
-
